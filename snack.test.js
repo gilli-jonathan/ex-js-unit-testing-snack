17737819035609b8e81206788186083cc09d70ce
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-04-13 21:13:39 +0200

    snack 2

--- snack.test.js
+++ snack.test.js
@@ -1,8 +1,18 @@
 const { getInitials } = require("./snack")
 
-
 //1 SNACK
 test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
     expect(getInitials('Budino Cioccolato')).toBe('B.C.')
     expect(getInitials('Jonathan gilli')).toBe('J.G.')
 })
+
+//2 SNACK
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+
+    expect(createSlug('CIOCCOLATOSOO')).toBe('cioccolatosoo')
+})
+
+function createSlug(stringa) {
+
+    return stringa.toLowerCase()
+}
